70e3d7564f4d2474669dc6ee839eb012370b13ec
alaurai <alauraivani@gmail.com>
2023-06-05 19:05:12 -0300

    feat: add template

---
+++ src/app.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const express = require('express')
+const nodemailer = require('nodemailer')
+const handlebars = require('handlebars');
+const hbs = require('nodemailer-express-handlebars')
+
+
+const app = express()
+const port = 3333
+
+const smtp = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'email@gmail.com',
+    pass: 'emailpassword'
+  },
+  tls: {
+    rejectUnauthorized: false,
+}
+})
+
+const handlebarOptions = {
+  viewEngine: {
+    extName: ".html",
+    partialsDir: path.resolve('./src/templates'),
+    defaultLayout: false,
+  },
+  viewPath: path.resolve('./src/templates'),
+  extName: ".html",
+}
+
+smtp.use('compile', hbs(handlebarOptions))
+
+handlebars.registerHelper('isIllustrator', function (variable) {
+  if ( variable === true) {
+    return true
+  } else {
+    return false
+  }
+});
+
+const configEmail = {
+  from: 'email@gmail.com',
+  to: 'email2@gmail.com',
+  subject: 'Templates de email MinhaCena',
+  template: 'email',
+  context: {
+    name: 'João Ilustrador',
+    isIllustrator: true
+  }
+}
+
+smtp.sendMail(configEmail, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email enviado: ' + info.response);
+  }
+});
+
+
+
+app.listen(port, () => {
+  console.log(`App is running at http://localhost:${port}`)
+})
